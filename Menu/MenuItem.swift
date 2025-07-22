6bd721b6564938682400942c813978b2d44eaca3
Kyle Morton <mkmorton91@gmail.com>
2025-07-21 20:02:22 -0500

    Adding data before adding list component

---
+++ Menu/MenuItem.swift
@@ -0,0 +1,17 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Kyle Morton on 7/21/25.
+//
+
+import Foundation
+
+struct MenuItem { // struct==class (can have props and behavior)
+    var name:String
+    var price:String
+    var imageName:String
+}
+
+//var firstItem = MenuItem(name: "Onigiri", price: "$1.99", imageName: "onigiri")
+//var secondItem = MenuItem(name: "Meguro Sushi", price: "$4.99", imageName: "meguro-sushi")
